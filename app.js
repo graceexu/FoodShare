85d800c6f2d4605b78cf7459fdfb23976d6140f3
Grace Xu <gracexucs@gmail.com>
2023-06-23 16:50:40 -0500

    Establish connection to MongoDB

--- app.js
+++ app.js
@@ -7,3 +7,22 @@ const MONGODB_URI = config.MONGODB_URI;
 
 const app = express();
 const port = 3000;
+
+// connect to mongo
+async function connectToDatabase() {
+    try {
+      await mongoose.connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+  
+      console.log('Connection to MongoDB successful.');
+  
+      // test connection by sending a ping
+      const adminDb = mongoose.connection.db.admin();
+      const pingResult = await adminDb.ping();
+      console.log('Pinged deployment. Successful connection to Mongo.');
+    } catch (error) {
+      console.error('Error connecting to MongoDB: ', error.message);
+    }
+}
